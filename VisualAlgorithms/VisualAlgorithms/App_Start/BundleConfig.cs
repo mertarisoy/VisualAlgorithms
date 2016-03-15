8a93cdc5ec60bb34e3ab2fe060343aa2fc03f331
Mert Arısoy <mertarisoy@gmail.com>
2016-03-16 00:46:14 +0200

    - Removed unused extensions. - Sytax Highlighting added. - Font Awesome added.

--- VisualAlgorithms/VisualAlgorithms/App_Start/BundleConfig.cs
+++ VisualAlgorithms/VisualAlgorithms/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace VisualAlgorithms
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/sb-admin.css",
+                      "~/Content/css/site.css"));
         }
     }
 }
