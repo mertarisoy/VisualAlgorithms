359d87bc85fa6d57955938bf7e18bfa4d5f0a8c8
mertarisoy <mertarisoy@gmail.com>
2016-05-27 02:01:12 +0300

    Localization Implementation.

---
+++ VisualAlgorithms/VisualAlgorithms.Common/ResManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualAlgorithms.Common
+{
+    public static class ResManager
+    {
+        public static string GetResource(string key)
+        {
+            
+            var myManager = new ResourceManager("VisualAlgorithms.Common.Resources.Strings", Assembly.GetExecutingAssembly());
+            string res = key;
+
+            var o = myManager.GetString(key, CultureInfo.CurrentCulture);
+            if (o != null) return o;
+
+            try
+            {
+                
+            }
+            catch
+            {
+                return null; 
+            }
+
+            return res;
+        }
+    }
+}
---
+++ VisualAlgorithms/VisualAlgorithms.Common/UserManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace VisualAlgorithms.Common
+{
+    public class UserManager
+    {
+        public static UserInfo CurrentUser { get; set; }
+
+        private static readonly string KEY_CODE = "UserInfo";
+        public static bool UserLoggedIn()
+        {
+
+            var context = HttpContext.Current;
+            var user = context.Session[KEY_CODE];
+            if (user == null)
+                return false;
+
+            return true;
+        }
+
+        public static void CreateSession(UserInfo user)
+        {
+            HttpContext.Current.Session[KEY_CODE] = user;
+            CurrentUser = user;
+        }
+
+        public static void DestroySession()
+        {
+            CurrentUser = null;
+            HttpContext.Current.Session.Clear();
+        }
+
+    }
+}
